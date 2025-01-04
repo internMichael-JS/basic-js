9a8cf7e14c7e7a8b9b49fa7625f29f2e2f958c85
Michail <astapenko.m99@gmail.com>
2025-01-05 01:01:43 +0300

    feat: done mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const newM = matrix.map(row => row.map(() => 0))
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      console.log('------------')
+      let prevPrewRow, abovePrevRow, nextPrewRow, prevNextRow, underNextRow, nextNextRow = null;
+      const prevCurrentRow = j > 0 ? matrix[i][j - 1] : null;
+      const nextCurrentRow = j + 1 < matrix[i].length ? matrix[i][j + 1] : null;
+      if (i > 0) {
+        prevPrewRow = j > 0 ? matrix[i - 1][j - 1] : null;
+        abovePrevRow = matrix[i - 1][j];
+        nextPrewRow = j + 1 < matrix[i].length ? matrix[i - 1][j + 1] : null;
+      }
+      if (i + 1 < matrix.length) {
+        prevNextRow = j > 0 ? matrix[i + 1][j - 1] : null;
+        underNextRow = matrix[i + 1][j];
+        nextNextRow = j + 1 < matrix[i].length ? matrix[i + 1][j + 1] : null;
+      }
+      const count = getCountOfTrue([prevPrewRow, abovePrevRow, nextPrewRow, prevNextRow, underNextRow, nextNextRow, prevCurrentRow, nextCurrentRow])
+      newM[i][j] = count
+    }
+  }
+  return newM
+}
+function getCountOfTrue(arr) {
+  return arr.filter(el => el === true).length
 }
 
 module.exports = {
