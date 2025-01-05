2e7f81a55a72292db01cb0847d0d2f36f02489b9
Michail <astapenko.m99@gmail.com>
2025-01-05 16:46:57 +0300

    feat: done vigenere-cipher

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,23 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+function dateSample(sampleActivity) {
+  if (isNaN(sampleActivity) || typeof sampleActivity !== 'string') return false;
+  let res = Math.log(MODERN_ACTIVITY / (+sampleActivity)) * HALF_LIFE_PERIOD / Math.log(2);
+  return (isNaN(res) || res === Infinity || res < 0) ? false : Math.ceil(res);
+}
 module.exports = {
   dateSample
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,64 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.process(message, key, 'encrypt');
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    return this.process(encryptedMessage, key, 'decrypt');
+  }
+  process(message, key, mode) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    const result = [];
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i];
+      if (alphabet.includes(char)) {
+        const messageIndex = alphabet.indexOf(char);
+        const keyChar = key[keyIndex % key.length];
+        const keyIndexValue = alphabet.indexOf(keyChar);
+        if (mode === 'encrypt') {
+          const encryptedIndex = (messageIndex + keyIndexValue) % alphabet.length;
+          result.push(alphabet[encryptedIndex]);
+        } else if (mode === 'decrypt') {
+          const decryptedIndex = (messageIndex - keyIndexValue + alphabet.length) % alphabet.length;
+          result.push(alphabet[decryptedIndex]);
+        }
+        keyIndex++;
+      } else {
+        result.push(char);
+      }
+    }
+    const finalResult = result.join('');
+    return this.isDirect ? finalResult : finalResult.split('').reverse().join('');
   }
 }
 
